47d77209585fc3c4d00ada70367808b144c1e3b6
thapadevendra <43578948+thapadevendra@users.noreply.github.com>
2020-07-15 11:40:32 -0500

    Divide files and added folder to categorised for our MVVM concept. Each files are classified as Model, View and ViewModel for future implementation.

---
+++ Tinker Weapons Challenge/Model/AirCraft.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tinker_Weapons_Challenge.Model
+{
+    //this class will instantiate the type of Aircraft we need
+    //need more to work for MVVM approach with databinding in a ViewModel
+    class AirCraft
+    {
+        string name;        //name of the AirCraft
+        int initAirCraftWeight;     //initial weight of the AirCraft
+        int fuelWeight;     // fuelWeight loaded
+        public AirCraft()       //empty constructor
+        {
+
+        }
+        public AirCraft(string name, int initAirCraftWeight, int fuelWeight)       // constructor  with parameter
+        {
+            this.name = name;
+            this.initAirCraftWeight = initAirCraftWeight;
+        }
+    }
+}
---
+++ Tinker Weapons Challenge/ViewModel/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tinker_Weapons_Challenge.ViewModel
+{
+    //This class will function as data binding between View and Model 
+    //It will interact as the interface between user response to interface
+    //and database behind the system
+    class UserViewModel
+    {
+    }
+}
